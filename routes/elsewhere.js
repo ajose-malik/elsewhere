6dcacb2288fb0032a6f2903a969fba97e4fb4671
ajose-malik <ajose.malik@gmail.com>
2020-12-30 22:52:30 -0500

    Create elsewhere basic CRUD

--- routes/elsewhere.js
+++ routes/elsewhere.js
@@ -2,6 +2,11 @@ const express = require('express');
 const elseRouter = express.Router();
 const Elsewhere = require('../models/elsewhere');
 
+elseRouter.get('/', async (req, res) => {
+	const elsewheres = await Elsewhere.find({});
+	res.render('elsewhere/index', { elsewheres });
+});
+
 elseRouter.get('/new', (req, res) => {
 	res.render('elsewhere/new');
 });
@@ -10,8 +15,33 @@ elseRouter.post('/', async (req, res) => {
 	const { elsewhere } = req.body;
 	const newElsewhere = new Elsewhere(elsewhere);
 	await newElsewhere.save();
-	console.log(elsewhere);
-	res.send('created!!!');
+	res.redirect(`/elsewhere/${newElsewhere.id}`);
+});
+
+elseRouter.get('/:id', async (req, res) => {
+	const { id } = req.params;
+	const elsewhere = await Elsewhere.findById(id);
+	res.render('elsewhere/show', { elsewhere });
+});
+
+elseRouter.get('/:id/edit', async (req, res) => {
+	const { id } = req.params;
+	const elsewhere = await Elsewhere.findById(id);
+	res.render('elsewhere/edit', { elsewhere });
+});
+
+elseRouter.put('/:id', async (req, res) => {
+	const { id } = req.params;
+	const elsewhere = await Elsewhere.findByIdAndUpdate(id, {
+		...req.body.elsewhere
+	});
+	res.redirect(`/elsewhere/${elsewhere.id}`);
+});
+
+elseRouter.delete('/:id', async (req, res) => {
+	const { id } = req.params;
+	await Elsewhere.findByIdAndDelete(id);
+	res.redirect('/elsewhere');
 });
 
 module.exports = elseRouter;
